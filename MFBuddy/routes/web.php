b7f85333bff07dde31f0104a9dab1e0245e46ec0
Han Khuu <h.khuu01@gmail.com>
2017-06-26 21:06:15 -0400

    added routes

--- MFBuddy/routes/web.php
+++ MFBuddy/routes/web.php
@@ -12,5 +12,18 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'My Fitness Buddy';
+    return view('welcome')->with('title', $title );
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/meals', 'MealsController@meals');
+
+Route::get('/meals/create', 'MealsController@mealsCreate');
+
+Route::get('/users/{id}', function($id) {
+    return $id;
 });
